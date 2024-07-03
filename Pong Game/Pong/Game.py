85da520d02390afefc41b0fd200314ced6d202e2
Gongas <goncalo.mesteves03@gmail.com>
2024-07-03 23:45:12 +0100

    Specified Input Types in the class Methods

--- Pong Game/Pong/Game.py
+++ Pong Game/Pong/Game.py
@@ -1,29 +1,28 @@
 from .Paddle import (Paddle)
 from .Ball import (Ball)
 import pygame
-import random
 from .Constants import (SCREEN_WIDTH, SCREEN_HEIGHT, WHITE, BLACK, RED)
 
 class GameInformation:
-    def __init__(self, left_hits=None, right_hits=None, left_score=None, right_score=None):
+    def __init__(self, left_hits:int=None, right_hits:int=None, left_score:int=None, right_score:int=None) -> None:
         self.left_hits = 0 if left_hits is None else left_hits
         self.right_hits = 0 if right_hits is None else right_hits
         self.left_score = 0 if left_score is None else left_score
         self.right_score = 0 if right_score is None else right_score
     
-    def _update_score(self, left=False):
+    def _update_score(self, left:bool=False) -> None:
         if left:
             self.left_score += 1
         else:
             self.right_score += 1
 
-    def _update_hits(self, left=False):
+    def _update_hits(self, left:bool=False) -> int:
         if left:
             self.left_hits += 1
         else:
             self.right_hits += 1
 
-    def __str__(self):
+    def __str__(self) -> str:
         return (f"LEFT PLAYER [{self.left_score}] : [{self.right_score}] RIGHT PLAYER")
 
 class Game:
@@ -34,9 +33,10 @@ class Game:
     Use the information returned from .loop() to determine when to end the game by calling
     .reset().
     """
+
     SCORE_FONT = pygame.font.SysFont("comicsans", 50)
 
-    def __init__(self):
+    def __init__(self) -> None:
         # Referencing the Game Elements
         self.left_paddle = Paddle(10, SCREEN_HEIGHT // 2 - Paddle.HEIGHT // 2)
         self.right_paddle = Paddle(SCREEN_WIDTH - 10 - Paddle.WIDTH, SCREEN_HEIGHT // 2 - Paddle.HEIGHT // 2)
@@ -45,24 +45,24 @@ class Game:
         # Creating an instance of Game Information to keep track of the scores and hits per player
         self.game_info = GameInformation()
 
-    def _draw_score(self, Window):
+    def _draw_score(self, Window:pygame.Surface) -> None:
         left_score_text = self.SCORE_FONT.render(f"{self.game_info.left_score}", 1, WHITE)
         right_score_text = self.SCORE_FONT.render(f"{self.game_info.right_score}", 1, WHITE)
         
         Window.blit(left_score_text, (SCREEN_WIDTH // 4 - left_score_text.get_width() // 2, 20))
         Window.blit(right_score_text, (SCREEN_WIDTH * (3/4) - right_score_text.get_width() // 2, 20))
 
-    def _draw_hits(self, Window):
+    def _draw_hits(self, Window:pygame.Surface) -> None:
         hits_text = self.SCORE_FONT.render(f"{self.game_info.left_hits + self.game_info.right_hits}", 1, RED)
         Window.blit(hits_text, (SCREEN_WIDTH // 2 - hits_text.get_width() // 2, 10))
 
-    def _draw_divider(self, Window):
+    def _draw_divider(self, Window:pygame.Surface) -> None:
         for i in range(10, SCREEN_HEIGHT, SCREEN_HEIGHT//20):
             if i % 2 == 1:
                 continue
             pygame.draw.rect(Window, WHITE, (SCREEN_WIDTH // 2 - 5, i, 10, SCREEN_HEIGHT // 20))
 
-    def _handle_collision(self):
+    def _handle_collision(self) -> None:
         ball = self.ball
         left_paddle = self.left_paddle
         right_paddle = self.right_paddle
@@ -94,22 +94,22 @@ class Game:
                     ball.y_vel = -1 * y_vel
                     self.game_info._update_hits(left=False)
 
-    def draw(self, Window, draw_score=True, draw_hits=False):
+    def draw(self, Window:pygame.Surface, draw_score:bool=True, draw_hits:bool=False) -> None:
         Window.fill(BLACK)
         self._draw_divider(Window)
 
         if draw_score:
-            self._draw_score()
+            self._draw_score(Window)
 
         if draw_hits:
-            self._draw_hits()
+            self._draw_hits(Window)
 
         for paddle in [self.left_paddle, self.right_paddle]:
             paddle.draw(Window)
 
         self.ball.draw(Window)
 
-    def move_paddle(self, left=True, up=True):
+    def move_paddle(self, left:bool=True, up:bool=True) -> bool:
         """
         Move the left or right paddle.
 
@@ -132,7 +132,7 @@ class Game:
 
         return True
 
-    def loop(self):
+    def loop(self) -> GameInformation:
         """
         Executes a single game loop.
 
@@ -152,16 +152,9 @@ class Game:
 
         return self.game_info
 
-    def reset(self):
+    def reset(self) -> None:
         """Resets the entire game."""
         self.ball.reset()
         self.left_paddle.reset()
         self.right_paddle.reset()
         self.game_info = GameInformation()
-
-if __name__ == "__main__":
-    pygame.init()
-    WINDOW = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    game = Game(WINDOW)
-    outcome = game.loop()
-    print(outcome)
