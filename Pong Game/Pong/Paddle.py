87c1facd7f7bf7c6e5c5f5c9f93aed6583f7899c
Gongas <goncalo.mesteves03@gmail.com>
2024-07-06 21:33:49 +0100

    Updated Paddle's Format and Color

--- Pong Game/Pong/Paddle.py
+++ Pong Game/Pong/Paddle.py
@@ -2,17 +2,17 @@ import pygame
 from .Constants import (WHITE, PADDLE_WIDTH, PADDLE_HEIGHT, PADDLE_VEL)
 
 class Paddle:
-    COLOR = WHITE # Color for the Paddles
     VEL = PADDLE_VEL # Velocity of the Paddles
     WIDTH = PADDLE_WIDTH
     HEIGHT = PADDLE_HEIGHT
 
-    def __init__(self, X:int, Y:int) -> None:
+    def __init__(self, X:int, Y:int, Color:tuple=None) -> None:
         self.initial_x = self.x = X
         self.initial_y = self.y = Y
+        self.COLOR = WHITE if Color is None else Color
 
     def draw(self, Window:pygame.Surface) -> None:
-        pygame.draw.rect(Window, self.COLOR, (self.x, self.y, self.WIDTH, self.HEIGHT))
+        pygame.draw.rect(Window, self.COLOR, (self.x, self.y, self.WIDTH, self.HEIGHT), 0, 8)
 
     def move(self, up:bool=True) -> None:
         if (up):
